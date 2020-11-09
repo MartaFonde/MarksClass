9445f79c68faf880d8ffa835956912f2a5444e65
martafonde <marta4510@hotmail.com>
2020-11-09 20:01:27 +0100

    v4

--- MarksClass/MarksClass/MuestraDatos.cs
+++ MarksClass/MarksClass/MuestraDatos.cs
@@ -111,10 +111,10 @@ namespace MarksClass
             }
         }
 
-        public static void NotasAsigAlumno(Aula a, int alu, int asig)
+        public static void NotasAsigAlumno(Aula a, int alum, int asig)
         {
             Console.Write("{0, 24} | {1,2}. {2,11} |\n", " ", asig, Enum.GetName(typeof(Asignaturas), asig));
-            Console.Write("{0,2}. {1, 20} | {2,15} |\n", alu, alum[alu], aula[alu, asig]);
+            Console.Write("{0,2}. {1, 20} | {2,15} |\n", alum, a.alumnos[alum], a[alum, asig]);
         }
 
         //public static void TablaNotas(Aula a, string[] alumnos, int numAsig, int posAlum = -1, int posAsig = -1)
